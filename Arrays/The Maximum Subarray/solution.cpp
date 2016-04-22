08a0663b1b0479b5dfe03bc7e92c9ea33549ce19
Rafał Kozik <rafal@rafalkozik.com>
2016-04-22 12:11:07 +0200

    Arrays/The Maximum Subarray

---
+++ Arrays/The Maximum Subarray/solution.cpp
@@ -0,0 +1,55 @@
+/*
+ * HackerRank - The Maximum Subarray
+ * Arrays
+ * https://www.hackerrank.com/challenges/maxsubarray
+ * Author: Rafal Kozik
+ */
+
+#include <iostream>
+#include <vector>
+#include <limits>
+
+#define LOG(x) std::cout << x << std::endl;
+
+int main(int argc, char **argv)
+{
+    std::ios::sync_with_stdio(false);
+    int T;
+    std::cin >> T;
+    while (T--)
+    {
+        int N;
+        std::cin >> N;
+        std::vector<int> data(N, 0);
+        for (int i = 0; i < N; ++i)
+        {
+            std::cin >> data[i];
+        }
+        int sum = 0;
+        int pos = 0;
+        int max = std::numeric_limits<int>::min();
+        for (int i = 0; i < N; ++i)
+        {
+            sum += data[i];
+            if (sum > max)
+            {
+                max = sum;
+            }
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+            if (data[i] > 0)
+            {
+                pos += data[i];
+            }
+        }
+        if (pos == 0)
+        {
+            pos = max;
+        }
+        LOG(max << " " << pos);
+    }
+    return 0;
+}
+
