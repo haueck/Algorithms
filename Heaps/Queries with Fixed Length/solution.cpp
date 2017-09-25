5c46ba080dee35fff3559b854330ca127c7b77c4
Rafał Kozik <rafal@rafalkozik.com>
2017-09-25 20:03:23 +0200

    Heaps/Queries with Fixed Length

---
+++ Heaps/Queries with Fixed Length/solution.cpp
@@ -0,0 +1,45 @@
+/*
+ *
+ *
+ * Author: Rafal Kozik
+ */
+
+#include <vector>
+#include <iostream>
+#include <queue>
+
+#define LOG(x) std::cout << x << std::endl
+
+int main()
+{
+    int n, q;
+    std::cin >> n >> q;
+    std::vector<int> data(n);
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> data[i];
+    }
+    while (q--)
+    {
+        int d;
+        std::cin >> d;
+        std::priority_queue<std::pair<int,int>> max_heap;
+        std::priority_queue<int,std::vector<int>,std::greater<int>> min_heap;
+        for (int i = 0; i < d; ++i)
+        {
+            max_heap.emplace(data[i], i);
+        }
+        min_heap.push(max_heap.top().first);
+        for (int i = d; i < n; ++i)
+        {
+            max_heap.emplace(data[i], i);
+            while (max_heap.top().second <= i - d)
+            {
+                max_heap.pop();
+            }
+            min_heap.push(max_heap.top().first);            
+        }
+        LOG(min_heap.top());
+    }
+}
+
