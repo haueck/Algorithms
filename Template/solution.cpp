29cac6034e4037f2edc026d000589bf1998e3887
Rafal Kozik <rafal@rafalkozik.com>
2018-05-20 11:54:10 +0200

    Template update

--- Template/solution.cpp
+++ Template/solution.cpp
@@ -8,14 +8,8 @@
 #include <vector>
 
 #define LOG(x) std::cout << x << std::endl
-#ifdef DEBUG
-    #define DBG(x) std::cerr << x << std::endl
-#else
-    #define DBG(x)
-#endif
 
 int main()
 {
-    std::ios::sync_with_stdio(false);
 }
 
