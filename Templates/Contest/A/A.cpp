1f633f04f76240d14e5d51171486de284506af61
Rafal Kozik <rafal@rafalkozik.com>
2019-01-16 15:42:42 +0100

    Added contest tamplate

---
+++ Templates/Contest/A/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define LOG(x) std::cout << x << std::endl
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int n;
+    std::cin >> n;
+    std::vector<int> d(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> d[i];
+    }
+}
+
